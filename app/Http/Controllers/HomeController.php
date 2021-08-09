d4b4e9afa37a42d87af1f2b7d936484c80c3bfde
tkadahal <tkadahal@gmail.com>
2021-08-09 11:55:06 +0545

    Challenge Accepted

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,16 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        // $movies = Movie::all()->sortByDesc(function($movie) {
+        //     return $movie->ratings->avg('rating');
+        // })->take(100);
+
+        $movies = Movie::with(['category', 'ratings'])
+            ->take(100)
+            ->get()
+            ->sortByDesc(function ($movie) {
+                return $movie->ratings->avg('rating');
+            });
 
         return view('home', compact('movies'));
     }
