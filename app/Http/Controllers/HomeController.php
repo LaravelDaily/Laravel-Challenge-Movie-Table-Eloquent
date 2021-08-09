5c301935b8d268e6cb998d0937a6b5d04fd8077c
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-08-09 13:38:18 +0800

    Optimization

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Movie;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        $movies = Movie::with(['ratings', 'category'])
+            ->limit(100)
+            ->get()
+            ->sortByDesc(function ($movie) {
+                return $movie->ratings->avg('rating');
+            });
 
         return view('home', compact('movies'));
     }
