681e1a9d8685bd581a0c7cb3193434b8ccdf1f76
nipunTharuksha <nipunth7@gmail.com>
2021-08-09 11:45:48 +0530

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
-
+        $movies = Movie::with('ratings','category')->withCount('ratings')
+            ->withAvg('ratings','rating')->take(100)->get();
+/*dd($movies[0]);*/
         return view('home', compact('movies'));
     }
 }
