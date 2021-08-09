aa7edbe58e2bc57768ce64e9d6cf92e6ff2492a9
Lewis Cowles <lewiscowles@me.com>
2021-08-09 23:23:57 +0100

    Refresh ratings hourly via Kernel Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +26,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command(function() {
+            \App\Models\Movie::updateAllRatingsCache(Carbon::now()->subHours(1));
+        })->hourly();
     }
 
     /**
