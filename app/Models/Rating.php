f6bd3c280205770d0c4ca383910daf757a473deb
shakhzodbek96 <shakhzodbek96@mail.ru>
2021-08-09 22:44:03 +0500

    Challenge Gafurovs version

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     use HasFactory;
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
