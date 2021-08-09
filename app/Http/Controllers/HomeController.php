b62b9c0b08659007adc6a3de9f02bd5e9546cb57
Long Tran <long.thanh.tran@gmail.com>
2021-08-09 15:40:11 +0700

    to have the query runs on postgresql as well

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
 //         })->take(100);
 
         // My better Eloquent
-        $movies = Rating::groupBy('movie_id')
+        $movies = Rating::groupBy(['movie_id', 'movies.title', 'categories.name', 'movies.release_year'])
             ->selectRaw('movies.title, categories.name as name, movies.release_year, avg(rating) as rating, count(rating) as vote')
             ->leftJoin('movies', 'movies.id', '=', 'ratings.movie_id')
             ->leftJoin('categories', 'movies.category_id', '=', 'categories.id')
@@ -29,7 +29,7 @@ class HomeController extends Controller
 //            ->selectRaw('movies.title, movies.release_year, categories.name as name, avg(rating) as rating, count(rating) as vote')
 //            ->leftjoin('movies', 'ratings.movie_id', '=', 'movies.id')
 //            ->leftjoin('categories', 'categories.id', '=', 'movies.category_id')
-//            ->groupBy('movie_id')
+//            ->groupBy('movie_id', 'movies.title', 'categories.name', 'movies.release_year')
 //            ->orderByRaw('AVG(rating) DESC')
 //            ->limit(100)->get();
 
