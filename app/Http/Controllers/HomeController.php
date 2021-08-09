b29c3edb83c4d12dc262d35fc09b6c6082b2cd3c
Thotsuya <55269430+Thotsuya@users.noreply.github.com>
2021-08-08 23:59:27 -0600

    Improving Eloquent Performance
    Improving Eloquent Performance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,18 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+
+
+        $movies = Movie::query() //Using query for the next functions to be indented
+            ->select('id','category_id','title','release_year')
+            ->with('category',function($query){
+                $query->select('id','name');
+            })
+            ->withAvg('ratings','rating')
+            ->withCount('ratings')
+            ->take(100)
+            ->get()
+            ->sortByDesc('ratings_avg_rating');
 
         return view('home', compact('movies'));
     }
