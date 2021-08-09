28a26e5a78525649f86d7a264e7b5dec7d7e7ee2
Tika Dahal <69100170+tkadahal@users.noreply.github.com>
2021-08-09 12:55:57 +0545

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,12 +12,22 @@ class HomeController extends Controller
         //     return $movie->ratings->avg('rating');
         // })->take(100);
 
-        $movies = Movie::with(['category', 'ratings'])
-            ->take(100)
-            ->get()
-            ->sortByDesc(function ($movie) {
-                return $movie->ratings->avg('rating');
-            });
+//         $movies = Movie::with(['category', 'ratings'])
+//             ->take(100)
+//             ->get()
+//             ->sortByDesc(function ($movie) {
+//                 return $movie->ratings->avg('rating');
+//             });
+        
+        // Changes made as pointed out by nipunTharuksha. Thanks nipunTharuksha. 
+        // I got my wrong. I was whole loading the ratings that loaded 1050 models. 
+        // Now need to change view too. So I have made changes to my view as well
+        $movies = Movie::with('category')
+                ->withAvg('ratings','rating')
+                ->withCount('ratings')
+                ->orderByDesc('ratings_avg_rating')
+                ->limit(100)
+                ->get();
 
         return view('home', compact('movies'));
     }
