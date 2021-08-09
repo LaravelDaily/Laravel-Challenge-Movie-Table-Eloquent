4e40421d24c9250918178b1b76ea83bb6d22e8ca
Konstantin <sergeef.kos@gmail.com>
2021-08-09 11:03:19 +0300

    2 queries and 110 models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        $movies = Movie::with(['category'])
+            ->withAvg('ratings', 'rating')
+            ->withCount('ratings')
+            ->orderByDesc('ratings_avg_rating')
+            ->limit(100)
+            ->get();
 
         return view('home', compact('movies'));
     }
