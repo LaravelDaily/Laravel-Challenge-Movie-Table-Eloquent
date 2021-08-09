03a023c9ee46721c50cce664fe0659c75c6d208c
ExodusDev <57896736+ExodusDev@users.noreply.github.com>
2021-08-09 20:34:52 +0200

    Modify query by using DB builder, Foreign key and Hydrate into models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        $movies = Movie::hydrate(DB::table('movies', 'm')
+        ->leftJoin('categories AS c', 'm.category_id', '=', 'c.id')
+        ->selectRaw('m.*, c.name AS category_name, AVG(r.rating) AS ratings_avg, COUNT(r.id) as ratings_count')
+        ->leftJoin('ratings AS r', 'r.movie_id', '=', 'm.id')
+        ->groupBy('m.id')
+        ->limit(100)
+        ->orderBy('ratings_avg')
+        ->get()->toArray());
 
         return view('home', compact('movies'));
     }
