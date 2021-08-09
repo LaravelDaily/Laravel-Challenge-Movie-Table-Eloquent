c0ba2c95fdf9f948dda48ed376cb7476ab6faf48
NathanaelGT <gordontanu2004@gmail.com>
2021-08-09 22:54:20 +0800

    1 Query, 4MB Memory Usage, 54ms Request Duration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        $movies = Movie::query()
+            ->select(
+                'movies.title',
+                'movies.release_year',
+                'categories.name as category_name',
+                DB::raw('avg(`ratings`.`rating`) as `rating_avg`'),
+                DB::raw('count(`ratings`.`rating`) as `rating_count`'),
+            )
+            ->join('categories', 'categories.id', '=', 'movies.category_id')
+            ->join('ratings', 'ratings.movie_id', '=', 'movies.id')
+            ->groupBy('movies.id')
+            ->orderByDesc('rating_avg')
+            ->orderBy('title')
+            ->take(100)
+            ->get();
 
         return view('home', compact('movies'));
     }
