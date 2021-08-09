47c2623395b67f9a9f2d37c737fd0803e6aeae35
a70n3 <prohibitous@gmail.com>
2021-08-09 18:15:10 +0800

    use db raw on query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
-
+use App\Models\Rating;
+use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        /**
+         * Original Query
+         *  */ 
+        //$movies = Movie::all()->sortByDesc(function($movie) {
+        //    return $movie->ratings->avg('rating');
+        //})->take(100);
+        $movies = Movie::join('ratings','movies.id','=','ratings.movie_id')
+                        ->join('categories','movies.category_id','=','categories.id')
+                        ->select('movies.*','categories.*',DB::raw('AVG(ratings.rating) as avg_rating,COUNT(*) as Votes'))
+                        ->groupBy('movies.id')
+                        ->orderByRaw('avg(ratings.rating) DESC')->take(100)->get();
 
         return view('home', compact('movies'));
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,10 +22,10 @@
                             @foreach ($movies as $movie)
                                 <tr>
                                     <td>{{ $loop->iteration }}. {{ $movie->title }}</td>
-                                    <td>{{ $movie->category->name }}</td>
-                                    <td>{{ $movie->release_year }}</td>
-                                    <td>{{ number_format($movie->ratings->avg('rating'), 2) }}</td>
-                                    <td>{{ $movie->ratings->count() }}</td>
+                                    <td>{{ $movie['name']}}</td> 
+                                    <td>{{ $movie['release_year'] }}</td>
+                                    <td>{{$movie['avg_rating'] }}</td> 
+                                    <td>{{ $movie['Votes'] }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
