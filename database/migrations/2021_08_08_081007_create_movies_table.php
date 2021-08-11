7191fdb92982b049fe9b0a18bea323db5b46f8ef
teobatman00 <teobatman00@gmail.com>
2021-08-11 19:19:42 +0700

    Finish

--- database/migrations/2021_08_08_081007_create_movies_table.php
+++ database/migrations/2021_08_08_081007_create_movies_table.php
@@ -15,7 +15,7 @@ class CreateMoviesTable extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
+            $table->foreignId('category_id')->constrained('categories');
             $table->string('title');
             $table->integer('release_year');
             $table->timestamps();
