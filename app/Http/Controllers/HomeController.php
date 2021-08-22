36c53f380674d2342f98bb3018ac1053a50afdeb
Freskim Veliu <freskim.veliu@tenton.co>
2021-08-22 13:23:54 +0200

    Improved eloquent query performance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        $movies = Movie::query()
+            ->with('category')
+            ->withAvg('ratings', 'rating')
+            ->withCount('ratings')
+            ->take(100)
+            ->latest('ratings_avg_rating')
+            ->get();
 
         return view('home', compact('movies'));
     }
