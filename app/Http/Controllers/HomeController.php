0e51d2debaf5847ddb1a44a105573f1b3a705b98
Agent 47 <agent4143@gamil.com>
2021-08-28 20:00:40 +0300

    Improved query perfomance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+        $movies = Movie::with('category')
+                        ->withCount('ratings') //ratings_count
+                        ->withAvg('ratings','rating') //ratings_avg_rating
+                        ->orderByDesc('ratings_avg_rating')
+                        ->take(100)
+                        ->get();
 
         return view('home', compact('movies'));
     }
