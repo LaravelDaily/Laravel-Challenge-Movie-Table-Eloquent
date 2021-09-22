f36483b21f39ddf1701648afd92312d22251b9ea
Victor Gonzalez Toledo <v.gonzalez@cotronic.es>
2021-09-22 12:35:15 +0200

    Povilas solution

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,12 +8,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::with('ratings','category')
+        $movies = Movie::with('category')
             ->withAvg('ratings', 'rating')
+            ->withCount('ratings')
             ->orderByDesc('ratings_avg_rating')
             ->take(100)
             ->get();
-
         return view('home', compact('movies'));
     }
 }
