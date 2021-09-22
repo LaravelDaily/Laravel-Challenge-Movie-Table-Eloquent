c7fa2158a9a6f839e8e610b55b2dfc246f41f31f
Victor Gonzalez Toledo <v.gonzalez@cotronic.es>
2021-09-22 12:36:04 +0200

    Povilas solution

---
+++ database/migrations/2021_09_21_151904_add_foreign_key_to_ratings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->foreign('movie_id')->references('id')->on('movies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            //
+        });
+    }
+}
