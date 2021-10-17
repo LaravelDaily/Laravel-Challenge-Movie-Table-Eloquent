13cfd733f149457d32a793ff6dc322b10c94c678
MBFouad <m7md.bahaa1@gmail.com>
2021-10-17 14:49:00 +0200

    first soulation 2 Queries and 110 Models && second soulation 1 Query and 100 Models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+//        $movies = Movie
+//            ::join('categories', 'categories.id', 'movies.category_id')
+//            ->select('*')
+//            ->withAvg('ratings', 'rating')
+//            ->withCount('ratings')
+//            ->take(100)->orderBy('ratings_avg_rating', 'desc')->get();
+
+        $movies = Movie
+            ::with('category')
+            ->withAvg('ratings', 'rating')
+            ->withCount('ratings')
+            ->take(100)->orderBy('ratings_avg_rating', 'desc')->get();
 
         return view('home', compact('movies'));
     }
