9ee3841612df225d2e5fd6e5086b2dae23810857
gene222-arch <genephillip222@gmail.com>
2022-04-11 12:01:36 +0800

    feat: remove caching

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,23 +9,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        if (! Cache::has('movies.index')) {
-            $movies = Cache::remember('movies.index', 86400, function () {
-                $movies = Movie::query()
-                    ->with('category')
-                    ->withAvg('ratings', 'rating')
-                    ->withCount('ratings')
-                    ->orderByDesc('ratings_avg_rating')
-                    ->take(100)
-                    ->get();
-    
-                return $movies;
-            });
-        }
-
-        if (Cache::has('movies.index')) {
-            $movies = Cache::get('movies.index');
-        }
+        $movies = Movie::query()
+            ->with('category')
+            ->withAvg('ratings', 'rating')
+            ->withCount('ratings')
+            ->orderByDesc('ratings_avg_rating')
+            ->take(100)
+            ->get();
 
         return view('home', compact('movies'));
     }
